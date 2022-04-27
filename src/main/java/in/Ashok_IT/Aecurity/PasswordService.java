1a92039d67e83dc930d26d5e4d6ab733f7bafe56
tushartoke <tushartoke123@gmail.com>
2022-04-28 01:25:14 +0530

    first commit

---
+++ src/main/java/in/Ashok_IT/Aecurity/PasswordService.java
@@ -0,0 +1,29 @@
+package in.Ashok_IT.Aecurity;
+
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
+
+public class PasswordService {
+	public static String Encode(String input) {
+		Encoder EC = Base64.getEncoder();
+		return EC.encodeToString(input.getBytes());
+	}
+	
+	public static String Decode(String str) {
+		Decoder DC = Base64.getDecoder();
+		byte[] Decoded_Byte_String = DC.decode(str);
+		return new String(Decoded_Byte_String);
+	}
+	
+	public static void main(String[] args) {
+		
+		String Original_String = new String("Ashok");
+		System.out.println("Original String: " + Original_String);
+		String str = Encode("Ashok");
+		System.out.println("Encoded String: " + str);
+		String decoded_str = Decode(str);
+		System.out.println("Decoded String: " + decoded_str);
+	}
+
+}
